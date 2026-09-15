aac12f763e5fb1ab7871f575324c920914212a91
TICNET\student3 <tky2402097@stu.o-hara.ac.jp>
2026-09-15 16:08:46 +0900

    ・PhysicsEngineの改良

--- DX11MyEngine/Source/Cpp/PhysicsEngine.cpp
+++ DX11MyEngine/Source/Cpp/PhysicsEngine.cpp
@@ -32,23 +32,23 @@ PhysicsEngine::~PhysicsEngine()
 bool PhysicsEngine::Setup()
 {    
     // 衝突判定の設定
-    m_pConfig = new btDefaultCollisionConfiguration();
+    m_pConfig = std::make_unique<btDefaultCollisionConfiguration>();
 
     // 衝突判定を管理
-    m_pDispatcher = new btCollisionDispatcher(m_pConfig);
+    m_pDispatcher = std::make_unique < btCollisionDispatcher>(m_pConfig.get());
 
     // 広域衝突判定
-    m_pBroadphase = new btDbvtBroadphase();
+    m_pBroadphase = std::make_unique <btDbvtBroadphase>();
 
     // 物理演算のソルバー
-    m_pSolver = new btSequentialImpulseConstraintSolver();
+    m_pSolver = std::make_unique <btSequentialImpulseConstraintSolver>();
 
     // 物理ワールド
-    m_pWorld = new btDiscreteDynamicsWorld(
-        m_pDispatcher,
-        m_pBroadphase,
-        m_pSolver,
-        m_pConfig
+    m_pWorld = std::make_unique <btDiscreteDynamicsWorld>(
+        m_pDispatcher.get(),
+        m_pBroadphase.get(),
+        m_pSolver.get(),
+        m_pConfig.get()
     );
 
     // 重力
@@ -62,7 +62,7 @@ bool PhysicsEngine::Setup()
             1.0f,
             50.0f
         ));
-
+    
     btTransform transform;
     transform.setIdentity();
     transform.setOrigin(btVector3(0.0f, -1.0f, 0.0f));
@@ -75,11 +75,13 @@ bool PhysicsEngine::Setup()
         motionState,
         groundShape
     );
+    btRigidBody* groundBody = new btRigidBody(info);
 
-    btRigidBody* groundBody =
-        new btRigidBody(info);
-
+    // リジッドボディを追加
+    // ポインタはこちらで削除する必要があるので、保持
     m_pWorld->addRigidBody(groundBody);
+
+    m_RBPtrs.push_back(groundBody);
 
     return true;
 }
@@ -109,4 +111,103 @@ bool PhysicsEngine::Shutdown()
 void PhysicsEngine::Update(float deltaTime)
 {
     m_pWorld->stepSimulation(deltaTime);
+}
+
+
+//*-----------------------------------------------------------------------------------------
+//*【?】ボックスシェイプ作成
+//*-----------------------------------------------------------------------------------------
+btBoxShape* PhysicsEngine::CreateShapeBox(const VECTOR3::VEC3 & boxHalfExtents)
+{
+    return new btBoxShape(btVector3(boxHalfExtents.x, boxHalfExtents.y, boxHalfExtents.z));
+}
+
+//*-----------------------------------------------------------------------------------------
+//*【?】球シェイプ作成
+//*-----------------------------------------------------------------------------------------
+btSphereShape* PhysicsEngine::CreateShapeSphere(float radius)
+{
+    return new btSphereShape(btScalar(radius));
+}
+
+//*-----------------------------------------------------------------------------------------
+//*【?】カプセルシェイプ作成
+//*-----------------------------------------------------------------------------------------
+btCapsuleShape* PhysicsEngine::CreateShapeCapsule(float radius, float height)
+{
+    return new btCapsuleShape(btScalar(radius), btScalar(height));
+}
+
+//*-----------------------------------------------------------------------------------------
+//*【?】円柱シェイプ作成
+//*-----------------------------------------------------------------------------------------
+btCylinderShape* PhysicsEngine::CreateShapeCylinder(const VECTOR3::VEC3 & halfExtents)
+{
+    return new btCylinderShape(btVector3(halfExtents.x, halfExtents.y, halfExtents.z));
+}
+
+//*-----------------------------------------------------------------------------------------
+//*【?】円錐シェイプ作成
+//*-----------------------------------------------------------------------------------------
+btConeShape* PhysicsEngine::CreateShapeCone(float radius, float height)
+{
+    return new btConeShape(btScalar(radius), btScalar(height));
+}
+
+//*-----------------------------------------------------------------------------------------
+//*【?】三角錐シェイプ作成
+//*-----------------------------------------------------------------------------------------
+btBU_Simplex1to4* PhysicsEngine::CreateShapePyramid(const std::array<VECTOR3::VEC3, 4> v4)
+{
+    btVector3 vertex[4] =
+    {
+        {v4[0].x,v4[0].y,v4[0].z},
+        {v4[1].x,v4[1].y,v4[1].z},
+        {v4[2].x,v4[2].y,v4[2].z},
+        {v4[3].x,v4[3].y,v4[3].z},
+    };
+    return new btBU_Simplex1to4(vertex[0], vertex[1], vertex[2], vertex[3]);
+}
+
+//*-----------------------------------------------------------------------------------------
+//*【?】三角形シェイプ作成
+//*-----------------------------------------------------------------------------------------
+btBU_Simplex1to4* PhysicsEngine::CreateShapeTriangle(const std::array<VECTOR3::VEC3, 3> v3)
+{
+    btVector3 vertex[3] =
+    {
+        {v3[0].x,v3[0].y,v3[0].z},
+        {v3[1].x,v3[1].y,v3[1].z},
+        {v3[2].x,v3[2].y,v3[2].z},
+    };
+    return new btBU_Simplex1to4(vertex[0], vertex[1], vertex[2]);
+}
+
+//*-----------------------------------------------------------------------------------------
+//*【?】線シェイプ作成
+//*-----------------------------------------------------------------------------------------
+btBU_Simplex1to4* PhysicsEngine::CreateShapeLine(const std::array<VECTOR3::VEC3, 2> v2)
+{
+    btVector3 vertex[2] =
+    {
+        {v2[0].x,v2[0].y,v2[0].z},
+        {v2[1].x,v2[1].y,v2[1].z},
+    };
+    return new btBU_Simplex1to4(vertex[0], vertex[1]);
+}
+
+//*-----------------------------------------------------------------------------------------
+//*【?】点シェイプ作成
+//*-----------------------------------------------------------------------------------------
+btBU_Simplex1to4* PhysicsEngine::CreateShapePoint(const VECTOR3::VEC3 & v1)
+{
+    return new btBU_Simplex1to4(btVector3(v1.x, v1.y, v1.z));
+}
+
+//*-----------------------------------------------------------------------------------------
+//*【?】凸包シェイプ作成
+//*-----------------------------------------------------------------------------------------
+btConvexHullShape* PhysicsEngine::CreateShapeConvexHull(const float* points, int numPoints, int stride)
+{
+    return new btConvexHullShape(points, numPoints, stride);
 }
--- DX11MyEngine/Source/Header/PhysicsEngine.h
+++ DX11MyEngine/Source/Header/PhysicsEngine.h
@@ -1,5 +1,24 @@
 #pragma once
 
+/// <summary>
+/// コリジョンの形状
+/// </summary>
+enum class COLLISION_SHAPE
+{
+	BOX,				// ボックス
+	SPHERE,				// 球
+	CAPSULE,			// カプセル
+	CYLINDER,			// 円柱
+	CONE,				// 円錐
+	PYRAMID,			// 三角錐
+	TRIANGLE,			// 三角形
+	LINE,				// 線
+	POINT,				// 点
+	CONVEX_HULL,		// 凸包
+	CONVEX_TRIANGLE,	// 凸面三角形
+	CONCAVE_TRIANGLE,	// 凹面三角形
+	COMPOUND,			// 形状の組み合わせ
+};
 
 // ***************************************************************************************
 // ---------------------------------------------------------------------------------------
@@ -14,11 +33,13 @@
 class PhysicsEngine
 {
 private:
-	class btDefaultCollisionConfiguration* m_pConfig;
-	class btCollisionDispatcher* m_pDispatcher;
-	class btBroadphaseInterface* m_pBroadphase;
-	class btSequentialImpulseConstraintSolver* m_pSolver;
-	class btDiscreteDynamicsWorld* m_pWorld;
+	std::unique_ptr<class btDefaultCollisionConfiguration> m_pConfig;
+	std::unique_ptr<class btCollisionDispatcher> m_pDispatcher;
+	std::unique_ptr<class btBroadphaseInterface> m_pBroadphase;
+	std::unique_ptr<class btSequentialImpulseConstraintSolver> m_pSolver;
+	std::unique_ptr<class btDiscreteDynamicsWorld> m_pWorld;
+
+	std::vector<class  btRigidBody*>m_RBPtrs;	// リジッドボディのポインタを保持
 
 public:
 	PhysicsEngine();
@@ -27,5 +48,21 @@ public:
 	bool Setup();
 	void Update(float deltaTime);
 	bool Shutdown();
+
+	void RegisterShape();
+
+private:
+	class btBoxShape* CreateShapeBox(const VECTOR3::VEC3& boxHalfExtents);
+	class btSphereShape* CreateShapeSphere(float radius);
+	class btCapsuleShape* CreateShapeCapsule(float radius, float height);
+	class btCylinderShape* CreateShapeCylinder(const VECTOR3::VEC3& halfExtents);
+	class btConeShape* CreateShapeCone(float radius, float height);
+	class btBU_Simplex1to4* CreateShapePyramid(const std::array<VECTOR3::VEC3, 4> v4);
+	class btBU_Simplex1to4* CreateShapeTriangle(const std::array<VECTOR3::VEC3, 3> v3);
+	class btBU_Simplex1to4* CreateShapeLine(const std::array<VECTOR3::VEC3, 2> v2);
+	class btBU_Simplex1to4* CreateShapePoint(const VECTOR3::VEC3& v1);
+	class btConvexHullShape* CreateShapeConvexHull(const float* points, int numPoints, int stride = sizeof(VECTOR3::VEC3));
+	class btConvexTriangleMeshShape* CreateShapeConvexTriangleMesh();
+	class btBvhTriangleMeshShape* CreateShapeBvhTriangleMesh();
 };
 
